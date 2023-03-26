5d88afaff9fe81a9380e29399ea3d35d08e2993d
siahliling <2202159@sit.singaporetech.edu.sg>
2023-03-26 21:20:04 +0800

    login

--- login.php
+++ login.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html>
     <head>
-        <!-- CSS -->
-        <link rel="stylesheet" href="css/styles.min.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="bootstrap/bootstrap.min.css"/>
-        <link rel="stylesheet" href="bootstrap/animation.css"/>
+        <link rel="stylesheet" href="bootstrap/styles.min.css"/>
 
         <!-- Bootstrap JS -->
         <script src="js/script.min.js"></script>
@@ -28,25 +24,31 @@
     <body>
         
         <section class="py-5">
-            <div class="container py-5">
-                <div class="row mb-4 mb-lg-5">
-                    <div class="col-md-8 col-xl-6 text-center mx-auto">
-                        <h1 class="display-1 fw-bold" style="color: rgb(0,192,163);">Login</h1>
-                        <h2 class="fw-bold">Welcome back to your fitness journey - log in and let's get moving!</h2>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 align-self-center">
+                        <h1 class="display-5 fw-bold text-start d-xxl-flex align-items-xxl-center" style="color: rgb(0,192,163);padding-right: 0px;">Login</h1>
+                        <h2 class="display-5 fw-bold text-start" style="padding-right: 0px;font-size: 23px;">Welcome back to your fitness journey - log in and let's get moving!</h2>
                     </div>
-                </div>
-                <div class="row d-flex justify-content-center">
-                    <div class="col-md-6 col-xl-4">
+                    <div class="col-md-6">
                         <div class="card">
                             <div class="card-body text-center d-flex flex-column align-items-center">
                                 <div class="bs-icon-xl bs-icon-circle bs-icon-primary shadow bs-icon my-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-person">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
                                     </svg></div>
                                 <form action="process_login.php" method="post">
-                                    <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-                                    <div class="mb-3"><input class="form-control" type="password" name="pwd" placeholder="Password"></div>
-                                    <div class="mb-3"><button class="btn btn-primary shadow d-block w-100" type="submit">Log in</button></div>
-                                    <a href="forget_password.php"><p class="text-muted">Forgot your password?</p></a>
+                                    <div class="mb-3">
+                                        <input class="form-control" type="email" name="email" placeholder="Email" required="" pattern="&quot;/^[a-zA-Z0-9.!#$%&amp;'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/&quot; required">
+                                    </div>
+                                    <div class="mb-3">
+                                        <input class="form-control" type="password" name="password" placeholder="Password" required="">
+                                    </div>
+                                    <div class="mb-3">
+                                        <button class="btn btn-primary shadow d-block w-100" type="submit">Login</button>
+                                    </div>
+                                    <p class="text-muted">Forgot your password?&nbsp;
+                                        <a href="forget_password.php">Click here</a>
+                                    </p>
                                 </form>
                             </div>
                         </div>
